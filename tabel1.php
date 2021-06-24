c03bf14d3040f1155d531ed3ba604430f4f719f7
belaekaputri <belaekaputri28@gmail.com>
2021-06-24 22:28:08 +0700

    menambahkan tabel

---
+++ tabel1.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+
+    }
+
+    table {
+        margin: 0 auto;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid #111;
+    }
+</style>
+
+<body>
+    <table>
+        <tr>
+            <th rowspan="2">No</th>
+            <th colspan="3">Hari</th>
+        </tr>
+        <tr>
+            <th>senin</th>
+            <th>selasa</th>
+            <th>rabu</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>senin</td>
+            <td>selasa</td>
+            <td>rabu</td>
+        </tr>
+    </table>
+    <br>
+    <table>
+        <tr>
+            <th rowspan="2">Jadwal</th>
+            <th colspan="2">Terlaksana </th>
+            <th colspan="2">Perubahan</th>
+            <th colspan="6">Urutan</th>
+        </tr>
+        <tr>
+            <th>Ya</th>
+            <th>Tidak</th>
+            <th>Ya</th>
+            <th>Tidak</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+        </tr>
+        <tr>
+            <td>Senin</td>
+            <td>Ya</td>
+            <td>Tidak</td>
+            <td>Ya</td>
+            <td>Tidak</td>
+            <td>Y</td>
+            <td>T</td>
+            <td>T</td>
+            <td>T</td>
+            <td>T</td>
+            <td>T</td>
+        </tr>
+    </table>
+    <br>
+    <table>
+        <tr>
+            <th>Kode Barang</th>
+            <th>Harga Barang</th>
+            <th>Jumlah</th>
+            <th>Bayar</th>
+        </tr>
+        <tr>
+            <td>B1</td>
+            <TD>2000</TD>
+            <td>2</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>B1</td>
+            <TD>2000</TD>
+            <td>2</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>B1</td>
+            <TD>2000</TD>
+            <td>2</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <th colspan="4">Jumlah Bayar Rp.</th>
+        </tr>
+    </table>
+    <br>
+    <table>
+        <tr>
+            <th rowspan="3">Kegiatan</th>
+            <th>senin</th>
+            <th>selasa</th>
+            <th>Rabu</th>
+            <th>Kamis</th>
+            <th>Jumat</th>
+            <th>Sabtu</th>
+        </tr>
+        <tr>
+            <th colspan="3">Jadwal</th>
+            <th colspan="3">Jadwal</th>
+        </tr>
+        <tr>
+            <th colspan="6">Kegiatan</th>
+        </tr>
+        <tr>
+            <td> mood
+            </td>
+            <td> mood
+            </td>
+            <td> mood
+            </td>
+            <td> mood
+            </td>
+            <td> mood
+            </td>
+            <td> mood
+            </td>
+            <td> mood
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
